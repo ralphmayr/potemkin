07759cd9b1bf3a233ed1d1cf82b938d989e7f759
Ralph Mayr <ralph.mayr@gmail.com>
2020-01-04 07:21:48 +0100

    Added command line args

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,36 @@
+import { OptionDefinition } from 'command-line-args';
+import { Section } from 'command-line-usage';
 import { Server, ServerOptions } from './server';
 
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+
+const optionDefinitions: OptionDefinition[] = [
+  { name: 'port', alias: 'p', type: Number, defaultValue: 1774 },
+  { name: 'help', alias: 'h', type: Boolean }
+];
+
+const usageSections: Section[] = [
+  { header: 'potemkin', content: 'A simple tool to mock JSON responses when testing web apps' },
+  {
+    header: 'Options', optionList: [
+      { ...optionDefinitions[0], typeLabel: '{underline number}', description: 'The tcp port that the server should listen on.' },
+      { ...optionDefinitions[1], description: 'Displays this help message.' }
+    ]
+  }
+];
+
 (async () => {
+  const args = commandLineArgs(optionDefinitions);
+
+  if (args.help || (!args.port || args.port <= 0 || args.port > 65535)) {
+    console.log(commandLineUsage(usageSections));
+    return;
+  }
+
   const options = new ServerOptions();
-  options.debug = true;
+  options.debug = false;
+  options.port = args.port;
+
   new Server(options).listen();
 })();
